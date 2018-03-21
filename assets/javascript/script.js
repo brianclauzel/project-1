c1acc4c1a5dc2278c92e8787ba60c18e80869653
davidz2018 <dzhomebiz@gmail.com>
2018-03-20 21:00:32 -0700

    added Mexican Peso to the API key and JS file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -16,7 +16,7 @@ var config = {
 
 //   var location = "";
   var queryURLOpenWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + "san+diego" + "&appid=cd03ae7d8279897013fd57ac14371c18";
-  var queryURLCurrencyLayer = "http://www.apilayer.net/api/live?access_key=e7309c7b642881620d0502e49a7380e5&currencies=EUR,GBP,CAD,CHF,JPY,AUD&source=USD&format=1";
+  var queryURLCurrencyLayer = "http://www.apilayer.net/api/live?access_key=e7309c7b642881620d0502e49a7380e5&currencies=EUR,GBP,CAD,CHF,JPY,AUD,MXN&source=USD&format=1";
 //   var queryURLGooglePlaces = "AIzaSyB_Xfhs13XHrV22XM0BH6cxMe3gu3yx4eg"
 
 //open weather API
@@ -84,6 +84,7 @@ $.ajax ({
     $(".CHF").html('$'+source.quotes.USDCHF)
     $(".JPY").html('$'+source.quotes.USDJPY)
     $(".AUD").html('$'+source.quotes.USDAUD)
+    $(".MXN").html('$'+source.quotes.USDMXN)
     
   }).catch(function(error) {
     console.log(error)
